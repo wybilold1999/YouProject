066f418930ae7b5062963f7cece7b5a4b25eea25
wangyuebin <wangyb@ecqun.com>
2017-11-16 12:34:05 +0800

    modify bug

--- app/src/main/java/com/youdo/karma/config/AppConstants.java
+++ app/src/main/java/com/youdo/karma/config/AppConstants.java
@@ -10,9 +10,9 @@ package com.youdo.karma.config;
  */
 public class AppConstants {
 	
-//	public static final String BASE_URL = "http://112.74.85.193/YouLoveServer/";
+	public static final String BASE_URL = "http://119.23.144.11/YouLoveServer/";
 //	public static final String BASE_URL = "http://192.168.1.100/YouLoveServer/";
-	public static final String BASE_URL = "http://10.0.108.206:8080/YouLoveServer/";
+//	public static final String BASE_URL = "http://10.0.108.206:8080/YouLoveServer/";
 
 	/**
 	 * 密码加密密匙
