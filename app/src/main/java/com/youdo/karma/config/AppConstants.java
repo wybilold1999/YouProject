70ef85626ac551ad7b81afb77bab074b4d3675e1
wangyuebin <wangyb@ecqun.com>
2017-12-19 10:49:41 +0800

    modify bug

--- app/src/main/java/com/youdo/karma/config/AppConstants.java
+++ app/src/main/java/com/youdo/karma/config/AppConstants.java
@@ -12,7 +12,7 @@ public class AppConstants {
 	
 	public static final String BASE_URL = "http://119.23.144.11/YouLoveServer/";
 //	public static final String BASE_URL = "http://192.168.1.100/YouLoveServer/";
-//	public static final String BASE_URL = "http://10.0.108.206:8080/YouLoveServer/";
+//	public static final String BASE_URL = "http://10.0.109.57:8080/YouLoveServer/";
 
 	/**
 	 * 密码加密密匙
